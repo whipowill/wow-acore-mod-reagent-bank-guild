d498adf493bd4afb45b289401ef32778b7c2dc17
biosfree <57522664+biosfree@users.noreply.github.com>
2024-08-20 18:41:47 +0300

    Update reagent_bank_account_NPC.sql

--- sql/world/base/reagent_bank_account_NPC.sql
+++ sql/world/base/reagent_bank_account_NPC.sql
@@ -3,16 +3,11 @@ SET
 @Entry = 290011,
 @Name = "Ling";
 @Title = "Reagent Banker"
-@NameRU = "Линг";
-@TitleRU = "Банкир реагентов"
 
 DELETE FROM `creature_template` WHERE `entry` = @Entry;
 DELETE FROM `creature_template_model` WHERE `CreatureID` = @Entry;
-DELETE FROM `creature_template_locale` WHERE `entry` = @Entry;
 
 INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
 (@Entry, @Name, @Title, NULL, 0, 6, 6, 0, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker_account');
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 (@Entry, 0, 15965, 1.0, 1.0, NULL);
-
-INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES (@Entry, 'ruRU', @NameRU, @TitleRU, NULL);
