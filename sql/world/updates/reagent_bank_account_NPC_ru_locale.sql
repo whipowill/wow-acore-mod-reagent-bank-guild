a66ddec28ba282e31500c4c0708e9e69b9359a9e
Bite Of Storm <57522664+biosfree@users.noreply.github.com>
2024-08-26 14:24:38 +0300

    Delete sql directory

--- sql/world/updates/reagent_bank_account_NPC_ru_locale.sql
+++
@@ -1,9 +0,0 @@
-
-SET
-@Entry = 290011,
-@Name = "Линг",
-@Title = "Банкир реагентов";
-
-DELETE FROM `creature_template_locale` WHERE `entry` = @Entry;
-
-INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES (@Entry, 'ruRU', @Name, @Title, NULL);
