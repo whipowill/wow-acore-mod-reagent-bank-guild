d5e8b7ad4a97434524569625ca57627808b41cda
Bite Of Storm <57522664+biosfree@users.noreply.github.com>
2024-08-26 18:57:11 +0300

    Update reagent_bank_account_NPC_ru_locale.sql

--- data/sql/db_world/reagent_bank_account_NPC_ru_locale.sql
+++ data/sql/db_world/reagent_bank_account_NPC_ru_locale.sql
@@ -1,9 +1,5 @@
+SET @Entry =290011, @Name ="Линг", @Title ="Банкир реагентов";
 
-SET
-@Entry = 290011,
-@Name = "Линг",
-@Title = "Банкир реагентов";
-
-DELETE FROM `creature_template_locale` WHERE `entry` = @Entry;
+DELETE FROM `creature_template_locale` WHERE `entry` =@Entry;
 
 INSERT INTO `creature_template_locale` (`entry`, `locale`, `Name`, `Title`, `VerifiedBuild`) VALUES (@Entry, 'ruRU', @Name, @Title, NULL);
